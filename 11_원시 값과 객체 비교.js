26afde2fc0cc26e34f2db8354cc022d847a8a024
ssangdon <tngustkdehs@naver.com>
2023-07-18 16:29:24 +0900

    2023/07/18

---
+++ 11_원시 값과 객체 비교.js
@@ -0,0 +1,15 @@
+let a = 1;
+a = 2;
+console.log(a);
+
+var str = "string";
+str[0] = "S";
+console.log(str);
+
+var score = 80;
+var copy = score;
+console.log(score);
+console.log(copy);
+score = 100;
+console.log(score);
+console.log(copy);
