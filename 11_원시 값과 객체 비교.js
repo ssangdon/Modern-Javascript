b88a494be528e6b3f614de332ba5f73b05cce5f8
ssangdon <tngustkdehs@naver.com>
2023-07-18 23:52:25 +0900

    2023/07/18

--- 11_원시 값과 객체 비교.js
+++ 11_원시 값과 객체 비교.js
@@ -1,15 +1,61 @@
-let a = 1;
-a = 2;
-console.log(a);
+// const 키워드를 사용해 선언한 변수는 재할당이 금지된다. 상수는 재할당이 금지된 변수일 뿐이다.
+const o = {};
+// const 키워드를 사용해 선언한 변수에 할당한 원시 값(상수)은 변경할 수 없다.
+// 하지만 const 키워드를 사용해 선언한 변수에 할당한 객체는 변경할 수 있다.
+o.a = 1;
+console.log(o);
 
 var str = "string";
 str[0] = "S";
-console.log(str);
+// 문자열은 원시 값이므로 변경할 수 없다. 이때 에러가 발생하지 않는다.
+console.log(str); // string
 
 var score = 80;
 var copy = score;
-console.log(score);
-console.log(copy);
+
+console.log(score); // 80
+console.log(copy); // 80
+
 score = 100;
-console.log(score);
-console.log(copy);
+
+console.log(score); // 100
+console.log(copy); // 80
+
+var ex1 = "Hello";
+var ex2 = ex1;
+ex1 = "KIKI";
+console.log(ex1);
+console.log(ex2);
+
+var person = {
+  name: "Lee",
+};
+// 프로퍼티 값 갱신
+person.name = "Kim";
+
+// 프로퍼티 동적 생성
+person.address = "Seoul";
+
+console.log(person); //{name: "Kim", address: "Seoul"}
+
+var person = {
+  name: "Lee",
+};
+var copy = person;
+
+console.log(copy === person); // true
+copy.name = "Kim";
+person.address = "Seoul";
+
+console.log(person); // {name: 'Kim', address: "Seoul"}
+console.log(copy); // {name: 'Kim', address: "Seoul"}
+
+var person1 = {
+  name: "Lee",
+};
+var person2 = {
+  name: "Lee",
+};
+
+console.log(person1 === person2); // false
+console.log(person1.name === person2.name); // true
