95526573fed04943de88e899f17433d8e2698244
ssangdon <tngustkdehs@naver.com>
2023-09-01 18:14:39 +0900

    2023/09/01

--- 33_7번째 데이터 타입 Symbol.js
+++ 33_7번째 데이터 타입 Symbol.js
@@ -1,3 +1,19 @@
 const mySymbol = Symbol();
 console.log(typeof mySymbol);
 console.log(mySymbol);
+
+const s1 = Symbol.for("mySymbol");
+const s2 = Symbol.for("mySymbol");
+const a1 = Symbol("mySymbol");
+const a2 = Symbol("mySymbol");
+
+console.log(s1 === s2);
+console.log(a1 === a2);
+
+const Direction = {
+  UP: Symbol("up"),
+  DOWN: Symbol("down"),
+  LEFT: Symbol("left"),
+  RIGHT: Symbol("right"),
+};
+console.log(Direction);
